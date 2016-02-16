8ffb4708488bda76e470cf74ab6f56a794840f13
Helen Cheng <hcheng@ucsd.edu>
2016-02-16 15:25:01 -0800

    Working on search results page

--- routes/search_results.js
+++ routes/search_results.js
@@ -3,3 +3,9 @@ var results = require('../search_results.json');
 exports.main = function(req, res){
 	res.render('search_results', results);
 };
+
+exports.search = function(req, res){
+	for(var i = 0; i < results.["search_results"].length; i++) {
+		//if(results[i].city == )
+	}
+}
