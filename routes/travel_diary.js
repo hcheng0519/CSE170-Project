9a4a53e60a90040b60b8b516dda5cf7f1a4a78b7
Anjelica Thang <amthang@ucsd.edu>
2016-02-17 10:36:15 -0800

    worked on menu options for user and non user

--- routes/travel_diary.js
+++ routes/travel_diary.js
@@ -1,3 +1,19 @@
+var data = require('../travel_diary.json');
+
 exports.main = function(req, res){
   	res.render('travel_diary')
+}
+
+exports.choose_diary = function(req, res){
+	if(req != null){
+		var person = req.body.person;
+		var description = "";
+		var
+	}
+
+	for(var i = 0; i < data["diaries"].length; i++){
+		if(data["diaries"][i].person == person){
+			data["diaries"].push(person);
+		}
+	}
 }
