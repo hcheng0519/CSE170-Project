e281f5d5b94dcce4437cf6418f0dee20fdb493ee
Anjelica Thang <amthang@ucsd.edu>
2016-02-19 01:15:41 +0000

    error message for user shows up if not correct login

--- routes/pro_land.js
+++ routes/pro_land.js
@@ -9,23 +9,19 @@ exports.checkLogin = function(req, res){
 	if(req != null){
 		var email = req.body.email;
 		var pass = req.body.password;
+		var errormsg = data["msg"][0].errormsg;
 		var flag = false;
-		var first = "";
-		var last = "";
 
 		for(var i = 0; i < data["users"].length; i++){
 			if(data["users"][i].email == email && data["users"][i].password == pass){
-				// first = data["users"][i].firstName;
-				// last = data["users"][i].lastName;
 				flag = true;
 			}
 		}
 		if (flag) {
-			data["current"].push(first+ " " + last);
 			res.render('pro_land');
 
 		} else {
-			res.render('index_proj');
+			res.render('signin', data);
 		}
 	}
 }
